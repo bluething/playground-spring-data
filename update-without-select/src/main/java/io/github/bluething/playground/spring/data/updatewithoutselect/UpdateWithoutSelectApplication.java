c396f60ff6024512049d9c61a6986ae7ca894a32
Habib Ali Machpud <habib.machpud@gmail.com>
2022-07-03 21:52:31 +0700

    Init

---
+++ update-without-select/src/main/java/io/github/bluething/playground/spring/data/updatewithoutselect/UpdateWithoutSelectApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.data.updatewithoutselect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateWithoutSelectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpdateWithoutSelectApplication.class, args);
+    }
+
+}
---
+++ update-without-select/src/test/java/io/github/bluething/playground/spring/data/updatewithoutselect/UpdateWithoutSelectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.data.updatewithoutselect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateWithoutSelectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
