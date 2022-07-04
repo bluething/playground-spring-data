a6ec3c02b480d9224cf272d33924dd521319aac5
Habib Ali Machpud <habib.machpud@gmail.com>
2022-07-04 13:53:48 +0700

    Update NPlusOneSelectTes.java
    Test class

--- update-without-select/src/test/java/io/github/bluething/playground/spring/data/updatewithoutselect/NPlusOneSelectTes.java
+++ update-without-select/src/test/java/io/github/bluething/playground/spring/data/updatewithoutselect/NPlusOneSelectTes.java
@@ -1,34 +1,41 @@
 package io.github.bluething.playground.spring.data.updatewithoutselect;
 
 import com.vladmihalcea.sql.SQLStatementCountValidator;
+import io.github.bluething.playground.spring.data.updatewithoutselect.persistence.PostCommentService;
 import io.github.bluething.playground.spring.data.updatewithoutselect.persistence.PostService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
 public class NPlusOneSelectTes {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private PostCommentService postCommentService;
+
     @BeforeEach
     public void beforeEach() {
         SQLStatementCountValidator.reset();
     }
 
-    @Sql(
-            scripts = "/init.sql",
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-    )
-    @Sql(
-            scripts = "/cleanup.sql",
-            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
-    )
     @Test
     public void findAllWithNPlusOneWillHaveThreeSelectQueries() {
         postService.findAllWithNPlusOne();
-        SQLStatementCountValidator.assertSelectCount(3);
+        SQLStatementCountValidator.assertSelectCount(2);
+    }
+
+    @Test
+    public void findAllWithFetchWillHaveOneSelectQuery() {
+        postService.findAllWithFetch();
+        SQLStatementCountValidator.assertSelectCount(1);
+    }
+
+    @Test
+    public void getCommentsByIdWillHaveOneSelectQuery() {
+        postCommentService.getCommentsById(1);
+        SQLStatementCountValidator.assertSelectCount(1);
     }
 }
